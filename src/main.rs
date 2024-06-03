c02af1f04f94824c5fedc1aaf2f4439dcb7b0571
ambrosia13 <aavyakumbhare@gmail.com>
2024-06-03 14:02:00 -0700

    Update default log filter

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-mod game;
-mod render_state;
+use env_logger::Env;
+
+pub mod game;
+pub mod render_state;
 
 fn main() {
-    env_logger::init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("warn"))
+        .filter_module("forget_me_not", log::LevelFilter::Info)
+        .init();
+
     pollster::block_on(game::run());
 }
