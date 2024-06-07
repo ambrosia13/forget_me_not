c8bc6c75bd2059fa2507df4224283ce86329e7ea
ambrosia13 <aavyakumbhare@gmail.com>
2024-06-07 16:18:59 -0700

    Hide window cursor

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -17,6 +17,13 @@ fn init_window() -> (EventLoop<()>, Arc<Window>) {
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
+    // Set the cursor grab mode to one that is supported by the system.
+    window
+        .set_cursor_grab(CursorGrabMode::Confined)
+        .or_else(|_| window.set_cursor_grab(CursorGrabMode::Locked))
+        .unwrap();
+    window.set_cursor_visible(false);
+
     let window = Arc::new(window);
 
     (event_loop, window)
